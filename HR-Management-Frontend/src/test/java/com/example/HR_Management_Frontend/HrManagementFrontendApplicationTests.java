3612c6edb5f93173982606727ed73911cc0dba9a
Tithaye Beri <tithaye.beri@gmail.com>
2026-05-03 11:56:51 +0530

    initial commit

---
+++ HR-Management-Frontend/src/test/java/com/example/HR_Management_Frontend/HrManagementFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HR_Management_Frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
